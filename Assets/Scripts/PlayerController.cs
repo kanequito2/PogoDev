68e27136277357939042bce2fc2a7a5fc4e9e826
ajcadenave <a.cadena517@pascualbravo.edu.co>
2022-10-26 21:34:06 -0500

    Se agrega el salto del jugador y control de camara incompleto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float bounceForce = 10.0f;
+    public float bounceForce = 20.0f;
+    public float jumpForce = 20.0f;
     public float horizontalForce = 1;
     public float horizontalInput;
     public float jumpInput;
     public bool isOnGround = false;
     public float xRange = 19.5f;
     public bool jumpCooldown;
+    public float jumpCooldownTime = 1;
 
     public bool jumpTrigger;
     public GameObject JumpTriggerGO;
     private JumpTrigger jumpTriggerScript;
-
-    public float timer = 0;
     private Rigidbody playerRB;
     // Start is called before the first frame update
     void Start()
@@ -27,40 +27,18 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        jumpInput = Input.GetAxis("Jump");
+    {        
         jumpTrigger = jumpTriggerScript.isTriggerUp;
-        if (isOnGround)
-        {
-            playerRB.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
-            playerRB.AddForce(Vector3.right * horizontalForce * horizontalInput, ForceMode.Impulse);
-            isOnGround = false;
-        }
-        if(transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-        if(jumpInput == 1 && jumpTrigger)
-        {
-            playerRB.AddForce(Vector3.up * 15, ForceMode.Impulse);
-        }
-        if(jumpInput == 1)
-        {
-            jumpCooldown = true;
-            StartCoroutine(JumpCountdownRoutine());
-        }
-        Debug.Log(timer);
-        timer = 0;
+        PlayerInputs();
+        InOnGroundDetection();
+        PlayerXBounds();
+        PlayerJump();
+        JumpCooldown();
     }
 
     IEnumerator JumpCountdownRoutine()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(jumpCooldownTime);
         jumpCooldown = false;
     }
 
@@ -69,7 +47,63 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-            timer += Time.deltaTime;
+        }
+    }
+    private void PlayerXBounds()
+    {
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+    private void InOnGroundDetection()
+    {
+        if (isOnGround)
+        {
+            UpwardForce(bounceForce);
+            SideForce(horizontalForce);
+            isOnGround = false;
+        }
+    }
+    private void UpwardForce(float force)
+    {
+        playerRB.velocity = new Vector3(playerRB.velocity.x, 0, playerRB.velocity.z);
+        playerRB.AddForce(Vector3.up * force, ForceMode.Impulse);
+    }
+    private void SideForce(float force)
+    {
+        playerRB.velocity = new Vector3(0 , playerRB.velocity.y, playerRB.velocity.z);
+        playerRB.AddForce(Vector3.right * force * horizontalInput, ForceMode.Impulse);
+    }
+    private void JumpCooldown()
+    {
+        if (jumpInput == 1)
+        {
+            Debug.Log("Entro al cooldown");
+            jumpInput = 0;
+            jumpCooldown = true;
+            StartCoroutine(JumpCountdownRoutine());
+        }
+    }
+    private void PlayerInputs()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (!jumpCooldown)
+        {
+            jumpInput = Input.GetAxis("Jump");
+        }
+        
+    }
+    private void PlayerJump()
+    {
+        if (jumpInput == 1 && jumpTrigger)
+        {
+            SideForce(horizontalForce);
+            UpwardForce(jumpForce);
         }
     }
 }
